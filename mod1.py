4087a28300ae80f0147478decd58ac705423d063
swetco777 <56336645+swetco777@users.noreply.github.com>
2019-10-09 12:27:15 +0530

    Add files via upload

---
+++ mod1.py
@@ -0,0 +1,14 @@
+s = "If Comrade Napoleon says it, it must be right."
+a = [100, 200, 300]
+# 
+# def foo(arg):
+#      print(f'arg = {arg}')
+
+class Foo:
+    pass
+
+def add(x,y):
+   return x + y
+
+print(s)
+print(a)
