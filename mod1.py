81482e2b33c5b158384829fbd1cab7a1d2d74f74
swetco777 <56336645+swetco777@users.noreply.github.com>
2019-12-13 10:18:18 +0530

    Add files via upload

--- mod1.py
+++ mod1.py
@@ -1,8 +1,9 @@
 s = "If Comrade Napoleon says it, it must be right."
 a = [100, 200, 300]
-# 
-# def foo(arg):
-#      print(f'arg = {arg}')
+ 
+def foo(arg):
+    
+    print(f'arg = {arg}')
 
 class Foo:
     pass
